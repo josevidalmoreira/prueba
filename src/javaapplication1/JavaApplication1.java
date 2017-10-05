66f138fe8c117dc2f2ae223d76c829d28443e877
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-10-05 09:45:33 +0200

    Inicio

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,17 @@
+
+package javaapplication1;
+
+/**
+ * @author jvidalmoreira
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Programa de saudo
+        System.out.print("un saudo\t a dam 1\n\n");
+    }
+    
+}
